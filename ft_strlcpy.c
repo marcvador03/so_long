8274af618cef3c72f280eff3629a7e28d8611a94
mfleury <mfleury@student.42barcelona.com>
2024-06-17 15:10:51 +0200

    memmove and strlcpy completed

---
+++ ft_strlcpy.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfleury <mfleury@student.42barcelona.      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/17 14:52:04 by mfleury           #+#    #+#             */
+/*   Updated: 2024/06/17 15:09:44 by mfleury          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < size)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (i == size)
+		dst[i] = src[i];
+	else
+		dst[i] = '\0';
+	return (i);
+}
