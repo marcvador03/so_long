cd51a30ea46b6eaf22c97a36c9145fd8c4337cb0
mfleury <mfleury@student.42barcelona.com>
2024-07-22 10:54:00 +0200

    ft_strchr.c: changed s[len + 1] to s[len] as s[len + 1] is outside the string

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/29 21:44:28 by mfleury           #+#    #+#             */
-/*   Updated: 2024/07/04 15:01:31 by mfleury          ###   ########.fr       */
+/*   Updated: 2024/07/22 10:52:51 by mfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,7 @@ char	*ft_strchr(const char *s, int c)
 			return (&((char *)s)[i]);
 		i++;
 	}
-	if (s[len + 1] == '\0')
-		return (&((char *)s)[len + 1]);
+	if (s[len] == '\0')
+		return (&((char *)s)[len]);
 	return (0);
 }
