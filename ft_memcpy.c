8379af8a1c74a854ceadcf19d4be1a79139c69fc
Marc Fleury <mfleury03@gmail.com>
2024-09-03 21:04:19 +0200

    Squashed 'libft/' content from commit 8bfa428
    git-subtree-dir: libft
    git-subtree-split: 8bfa4285f3727730ee2e265bd00ee88019db9a55

---
+++ ft_memcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 21:41:33 by mfleury           #+#    #+#             */
+/*   Updated: 2024/07/16 12:15:25 by mfleury          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+		i++;
+	}
+	return (dest);
+}
---
+++ ft_strjoin.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfleury <mfleury@student.42barcelona.com>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/29 21:45:42 by mfleury           #+#    #+#             */
+/*   Updated: 2024/07/22 10:49:26 by mfleury          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*str;
+	int		len;
+	int		s1_len;
+	int		i;
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	len = s1_len + ft_strlen(s2);
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (i < s1_len)
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	while (i < len)
+	{
+		str[i] = s2[i - s1_len];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
